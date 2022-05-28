85331f6f1aed6deedc63b0abb589c6a8324e614d
qin-gs <3210968849@qq.com>
2022-05-28 20:03:03 +0800

    服务注册

--- commerce-nacos-client/src/main/java/com/example/commerce/NacosClientApplication.java
+++ commerce-nacos-client/src/main/java/com/example/commerce/NacosClientApplication.java
@@ -2,9 +2,10 @@ package com.example.commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
 public class NacosClientApplication {
 
     public static void main(String[] args) {
