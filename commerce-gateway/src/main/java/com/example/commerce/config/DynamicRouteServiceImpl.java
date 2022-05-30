7040d58eb9f36cb589d2d0a8ea03489e10bcd1a6
qin-gs <3210968849@qq.com>
2022-05-30 19:51:15 +0800

    注册网关监听器

---
+++ commerce-gateway/src/main/java/com/example/commerce/config/DynamicRouteServiceImpl.java
@@ -0,0 +1,114 @@
+package com.example.commerce.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+/**
+ * 网关事件监听器
+ * 事件推送：动态更新路由网关
+ */
+@Slf4j
+@Service
+@SuppressWarnings("all")
+public class DynamicRouteServiceImpl implements ApplicationEventPublisherAware {
+    /**
+     * 写路由定义
+     */
+    private final RouteDefinitionWriter routeDefinitionWriter;
+    /**
+     * 获取路由定义
+     */
+    private final RouteDefinitionLocator routeDefinitionLocator;
+    /**
+     * 事件发布
+     */
+    private ApplicationEventPublisher publisher;
+
+    public DynamicRouteServiceImpl(RouteDefinitionWriter routeDefinitionWriter, RouteDefinitionLocator routeDefinitionLocator) {
+        this.routeDefinitionWriter = routeDefinitionWriter;
+        this.routeDefinitionLocator = routeDefinitionLocator;
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        // 完成事件推送句柄初始化
+        this.publisher = applicationEventPublisher;
+    }
+
+    /**
+     * 增加路由定义
+     */
+    public String addRouteDefinition(RouteDefinition definition) {
+        try {
+            log.info("gateway add route: {}", definition);
+            // 保存路由配置并发布
+            routeDefinitionWriter.save(Mono.just(definition)).subscribe();
+            // 发布事件通知给 gateway，同步新增路由定义
+            this.publisher.publishEvent(new RefreshRoutesEvent(this));
+            return "success";
+        } catch (Exception e) {
+            log.error("addRouteDefinition error", e);
+            return "error";
+        }
+    }
+
+    public String updateList(List<RouteDefinition> definitions) {
+        log.info("gateway update route list: {}", definitions);
+        List<RouteDefinition> routeDefinitions = this.routeDefinitionLocator.getRouteDefinitions().buffer().blockFirst();
+        if (routeDefinitions.isEmpty()) {
+            // 清除之前所有的路由配置
+            routeDefinitions.forEach(routeDefinition -> {
+                this.deleteById(routeDefinition.getId());
+            });
+        }
+        // 更新路由定义同步到 gateway
+        definitions.forEach(routeDefinition -> {
+            this.addRouteDefinition(routeDefinition);
+        });
+        return "success";
+    }
+
+    /**
+     * 更新路由定义
+     */
+    public String updateRouteDefinition(RouteDefinition definition) {
+        try {
+            log.info("gateway update route: {}", definition);
+            // 保存路由配置
+            routeDefinitionWriter.delete(Mono.just(definition.getId()));
+            routeDefinitionWriter.save(Mono.just(definition)).subscribe();
+            // 发布事件通知给 gateway，同步更新路由定义
+            this.publisher.publishEvent(new RefreshRoutesEvent(this));
+            return "success";
+        } catch (Exception e) {
+            log.error("updateRouteDefinition error id {} {}", definition.getId(), e);
+            return "error " + definition.getId();
+        }
+    }
+
+    /**
+     * 删除路由定义
+     */
+    public String deleteById(String id) {
+        try {
+            log.info("gateway delete route: {}", id);
+            // 删除路由配置并发布
+            routeDefinitionWriter.delete(Mono.just(id)).subscribe();
+            // 发布事件通知给 gateway，同步删除路由定义
+            this.publisher.publishEvent(new RefreshRoutesEvent(this));
+            return "success";
+        } catch (Exception e) {
+            log.error("deleteRouteDefinition error", e);
+            return "error";
+        }
+    }
+}
---
+++ commerce-gateway/src/main/java/com/example/commerce/config/DynamicRouteServiceImplByNacos.java
@@ -0,0 +1,124 @@
+package com.example.commerce.config;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
+/**
+ * 通过 nacos 下发的动态路由配置，监听 nacos 中的配置变更，并实时更新到 DynamicRouteServiceImpl 中
+ */
+@Slf4j
+@Service
+@DependsOn("gatewayConfig")
+public class DynamicRouteServiceImplByNacos {
+
+    public static final ObjectMapper MAPPER = new ObjectMapper();
+    /**
+     * nacos 配置服务客户端
+     */
+    private ConfigService configService;
+    private final DynamicRouteServiceImpl dynamicRouteService;
+
+    public DynamicRouteServiceImplByNacos(DynamicRouteServiceImpl dynamicRouteService) {
+        this.dynamicRouteService = dynamicRouteService;
+    }
+
+    /**
+     * 连接到 nacos 之后，初始化默认配置
+     */
+    @PostConstruct
+    public void init() {
+        log.info("init gateway nacos config service");
+        try {
+            // 初始化 nacos 客户端
+            configService = initConfigService();
+            if (configService == null) {
+                log.error("init gateway nacos config service error");
+                return;
+            }
+            // 通过 nacos config 指定路由配置路径去获取路由配置
+            String config = configService.getConfig(GatewayConfig.NACOS_ROUTE_DATA_ID, GatewayConfig.NACOS_ROUTE_GROUP, GatewayConfig.DEFAULT_TIMEOUT);
+            List<RouteDefinition> routeDefinitions = MAPPER.readValue(config, new TypeReference<List<RouteDefinition>>() {
+            });
+
+            if (!routeDefinitions.isEmpty()) {
+                routeDefinitions.forEach(routeDefinition -> {
+                    log.info("init gateway nacos route definition {}", routeDefinition);
+                    dynamicRouteService.addRouteDefinition(routeDefinition);
+                });
+            }
+
+        } catch (NacosException e) {
+            log.error("init gateway nacos error {}", e.getMessage(), e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        // 设置监听器 (监听之后的变化)
+        dynamicRouteByNacosListener(GatewayConfig.NACOS_ROUTE_DATA_ID, GatewayConfig.NACOS_ROUTE_GROUP);
+    }
+
+    /**
+     * 初始化
+     */
+    private ConfigService initConfigService() {
+        try {
+            Properties properties = new Properties();
+            properties.setProperty("serverAddr", GatewayConfig.NACOS_SERVER_ADDR);
+            properties.setProperty("namespace", GatewayConfig.NACOS_NAMESPACE);
+
+            return configService = NacosFactory.createConfigService(properties);
+        } catch (NacosException e) {
+            log.error("init gateway nacos error {}", e.getMessage(), e);
+            return null;
+        }
+    }
+
+    /**
+     * 监听 nacos 下发的动态路由配置信息
+     */
+    private void dynamicRouteByNacosListener(String dataId, String group) {
+        try {
+            // 给 nacos 客户端添加监听器 (监听到配置更改同步到 gateway 中，实现动态路由效果)
+            configService.addListener(dataId, group, new Listener() {
+                /**
+                 * 可以自己提供线程池 (不提供就使用默认线程池)
+                 */
+                @Override
+                public Executor getExecutor() {
+                    return null;
+                }
+
+                /**
+                 * 收到配置变更信息
+                 */
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    try {
+                        log.info("receive configInfo {}", configInfo);
+                        List<RouteDefinition> definitions = MAPPER.readValue(configInfo, new TypeReference<List<RouteDefinition>>() {
+                        });
+                        dynamicRouteService.updateList(definitions);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        } catch (NacosException e) {
+            log.error("dynamicRouteByNacosListener error {}", e.getMessage(), e);
+        }
+    }
+}
